6b4e8ea9d9a4ed772c0283de0ba3674333d0c711
felipesora <felipeusora@gmail.com>
2026-03-05 15:10:03 -0300

    criando microsservico de usuarios

---
+++ src/main/java/br/com/techmarket_identity_service/TechmarketIdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.techmarket_identity_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmarketIdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechmarketIdentityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/techmarket_identity_service/TechmarketIdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techmarket_identity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmarketIdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
